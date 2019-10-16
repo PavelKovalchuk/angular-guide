b4bf5f840a2a9055ed5183019d4a683c2d8d2449
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-16 10:19:11 +0300

    21/ event bindings

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,31 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', // as element: <app-servers></app-servers>
   // selector: '[app-servers]', // as attribute: <div app-servers ></div>
   // selector: '.app-servers', // as class: <div class="app-servers"></div>
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: "./servers.component.html",
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  serverName = "";
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!";
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
