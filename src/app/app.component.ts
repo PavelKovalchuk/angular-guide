88c54fb4716527f23925343bef9bb47deeb88cd1
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-17 11:30:46 +0300

    start 7 module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers = [
-    {serverId: 10, name: "First", status: "online", content: "content 1111"},
-    // {serverId: 12, name: "Second", status: "offline", content: "content 2222"}
-  ];
-
-  onServerAdded(serverData: {name: string, status: string, content: string}) {
-     this.servers.push({
-      serverId: this.servers.length + 1,
-      name: serverData.name,
-      status: serverData.status,
-      content: serverData.content,
-    });
-  }
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
