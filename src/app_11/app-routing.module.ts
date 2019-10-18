88e638d72418b93949dd916d966673fb5572711a
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-18 18:05:52 +0300

    close module 11

--- src/app_11/app-routing.module.ts
+++ src/app_11/app-routing.module.ts
@@ -4,14 +4,14 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
 import { UsersComponent } from "./users/users.component";
-import { ServersComponent } from "./servers/servers.component";
+import { ServersComponent } from "./servers/servers.component
 import { UserComponent } from "./users/user/user.component";
-import { EditServerComponent } from "./servers/edit-server/edit-server.component";
-import { ServerComponent } from "./servers/server/server.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component
+import { ServerComponent } from "./servers/server/server.component
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
-import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
-import { ServerResolver } from './servers/server/server-resolver.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service
+import { ServerResolver } from './servers/server/server-resolver.service
 
 
 const appRoutes: Routes = [
--- src/app_11/app.module.ts
+++ src/app_11/app.module.ts
@@ -6,17 +6,17 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { UsersComponent } from "./users/users.component";
-import { ServersComponent } from "./servers/servers.component";
+import { ServersComponent } from "../app_11/servers/servers.componentomponent";
 import { UserComponent } from "./users/user/user.component";
-import { EditServerComponent } from "./servers/edit-server/edit-server.component";
-import { ServerComponent } from "./servers/server/server.component";
-import { ServersService } from "./servers/servers.service";
+import { EditServerComponent } from "../app_11/servers/edit-server/edit-server.componentomponent";
+import { ServerComponent } from "../app_11/servers/server/server.componentomponent";
+import { ServersService } from "../app_11/servers/servers.service.service";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthGuard } from "./auth-guard.service";
-import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { CanDeactivateGuard } from "../app_11/servers/edit-server/can-deactivate-guard.service.service";
 import { ErrorPageComponent } from "./error-page/error-page.component";
-import { ServerResolver } from "./servers/server/server-resolver.service";
+import { ServerResolver } from "../app_11/servers/server/server-resolver.service.service";
 
 @NgModule({
   declarations: [
