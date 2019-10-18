0346caa76cf45d2b72a9cdf381bb96e624f65b80
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-18 10:38:28 +0300

    queryParamsHandling

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 import { ServersService } from "../servers.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: "app-edit-server",
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: { id: number; name: string; status: string };
   serverName = "";
   serverStatus = "";
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -21,11 +22,10 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     // console.log("queryParams", this.route.snapshot.queryParams);
 
-    this.route.queryParams.subscribe(
-      (param) => {
-        // console.log("param ", param);
-      }
-    );
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      // console.log("param ", param);
+      this.allowEdit = queryParams["allowEdit"] === "1" ? true : false;
+    });
     this.route.fragment.subscribe();
 
     this.server = this.serversService.getServer(1);
