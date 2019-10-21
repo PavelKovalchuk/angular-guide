e78e52f01d7900465807d3fdcee66aef95e72359
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-21 15:54:37 +0300

    finish module 17

--- src/app_17/app.module.ts
+++ src/app_17/app.module.ts
@@ -1,14 +1,23 @@
+import { ShortenPipe } from './shorten.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ShortenPipe,
+    FilterPipe,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
