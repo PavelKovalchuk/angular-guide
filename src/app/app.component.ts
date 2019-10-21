e9ed3d84afcc0adeecc2542e8f4ac3a26b2f7111
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-21 11:54:17 +0300

    custom validations for the fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,13 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ["kovalchuk", "pavel"];
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     this.signupForm = new FormGroup({
       "userData": new FormGroup({
-        "username": new FormControl(null, Validators.required),
+        "username": new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         "email": new FormControl(null, [Validators.required, Validators.email]),
       }),
       "gender": new FormControl("male"),
@@ -31,6 +32,13 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get("hobbies")).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[key: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {"nameIsForbidden": true};
+    }
+    return null;
   }
 
 }
